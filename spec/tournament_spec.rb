c67a3693d38f01ad0cd65c525335dc7558a0be6a
aaz <akbar.zamir@gmail.com>
2012-02-22 18:00:50 +0000

    Basic round robin tournament capability.

--- spec/tournament_spec.rb
+++ spec/tournament_spec.rb
@@ -1,5 +1,22 @@
 require_relative '../tournament'
+require_relative '../tit4tat'
 
 describe Tournament do
-  it 'should be played as a series of round robin contests'
+  context 'with 5 tit4tat players' do
+    before(:each) do
+      @players = []
+      5.times {@players.push(Tit4Tat.new)}
+      @tournament = Tournament.new(@players)
+      @tournament.run
+    end
+    it 'should involve 10 contests' do
+      @tournament.contests.size.should == 10
+    end
+    it 'should result in 30 points per player' do
+      @tournament.contests.each do |contest|
+        contest.player_one_score.should == 30
+        contest.player_two_score.should == 30
+      end
+    end
+  end
 end
--- tournament.rb
+++ tournament.rb
@@ -1,2 +1,24 @@
+require_relative 'contest'
+
 class Tournament
+  def initialize(players)
+    @players = players
+    @contests = []
+  end
+  
+  def contests
+    @contests
+  end
+  
+  def run
+    # Round robin contests
+    while (@players.size >= 2) do
+      player = @players.shift
+      @players.each do |opponent|
+        contest = Contest.new(player, opponent, 10)
+        contest.play
+        @contests.push contest
+      end
+    end
+  end
 end
