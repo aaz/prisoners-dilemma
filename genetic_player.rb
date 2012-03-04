12e11ceb3a8ca446d8ca7d683b04409356b67146
Akbar Zamir <akbar.zamir@gmail.com>
2012-03-04 22:03:36 +0000

    Genetic code uses opponent's last 3 moves to determine response (#182857)

--- genetic_player.rb
+++ genetic_player.rb
@@ -1,12 +1,48 @@
 class GeneticPlayer
   def initialize(code)
-    if code.length != 15
-      raise ArgumentError.new("Genetic code length is not 15")
+    if code.length != 11
+      raise ArgumentError.new("Genetic code length is not 11")
     end
     code.each_char do |char|
       if char != 'C' and char != 'D' then
         raise ArgumentError.new("Genetic code contains something other than 'C' or 'D'")
       end
     end
+    @code = code
+    @history = []
+    # First three letters encode initial conditions
+    @code[0..2].each_char do |char|
+      if char == 'C' then
+        @history.push :cooperation
+      else
+        @history.push :defection
+      end
+    end
+  end
+  
+  def decision
+    case @history.last 3
+    when [:cooperation, :cooperation, :cooperation]
+      letter = @code[3]
+    when [:cooperation, :cooperation, :defection]
+      letter = @code[4]
+    when [:cooperation, :defection, :cooperation]
+      letter = @code[5]
+    when [:cooperation, :defection, :defection]
+      letter = @code[6]
+    when [:defection, :cooperation, :cooperation]
+      letter = @code[7]
+    when [:defection, :cooperation, :defection]
+      letter = @code[8]
+    when [:defection, :defection, :cooperation]
+      letter = @code[9]
+    when [:defection, :defection, :defection]
+      letter = @code[10]
+    end
+    return letter == 'C' ? :cooperation : :defection
+  end
+  
+  def experience(counterparty_decision)
+    @history.push counterparty_decision
   end
 end
--- spec/genetic_player_spec.rb
+++ spec/genetic_player_spec.rb
@@ -3,18 +3,62 @@ require_relative '../genetic_player'
 describe GeneticPlayer do
   it "should be created with genetic code containing only Cs and Ds" do
     expect {
-      GeneticPlayer.new('CCCCDDDDDCCCF')
+      GeneticPlayer.new('CCCDFDCCCDD')
     }.to raise_error(ArgumentError)
   end
-  it "should have a genetic code of precisely 15 characters" do
+  it "should have a genetic code of precisely 11 characters" do
     expect {
       GeneticPlayer.new('')
     }.to raise_error(ArgumentError)
     expect {
-      GeneticPlayer.new('CDCDCDCDCDCDCDCD')
+      GeneticPlayer.new('CDCDCDCDCD')
     }.to raise_error(ArgumentError)
   end
-  context "with genetic code 'CCCCCCCCCCCCCCC'" do
-    it "should cooperate initially"
+  # Encoded strategy: unconditionally cooperate
+  context "with genetic code 'CCCCCCCCCCC'" do
+    before(:each) do
+      @player = GeneticPlayer.new('CCCCCCCCCCC')
+    end
+    it "should cooperate initially" do
+      @player.decision.should eql :cooperation
+    end
+    context "when confronted with three consecutive defections" do
+      it "should still cooperate" do
+        3.times{@player.experience :defection}
+        @player.decision.should eql :cooperation
+      end
+    end
+    context "when presented with three consecutive cooperations" do
+      it "should reciprocate" do
+        3.times{@player.experience :cooperation}
+        @player.decision.should eql :cooperation
+      end
+    end
+  end
+  # Encoded strategy: defect when opponent defects at least twice in last three
+  # Assumes cooperation-cooperation-cooperation as initial condition
+  context "with genetic code 'CCCCCCDCDDD'" do
+    before(:each) do
+      @player = GeneticPlayer.new('CCCCCCDCDDD')
+    end
+    it "should cooperate initially" do
+      @player.decision.should eql :cooperation
+    end
+    context "when exposed to defection, cooperation, defection" do
+      it "should defect" do
+        @player.experience :defection
+        @player.experience :cooperation
+        @player.experience :defection
+        @player.decision.should eql :defection
+      end
+    end
+  end
+  # Encoded strategy: defect when opponent defects at least twice in last three
+  # Assumes defection-cooperation-defection as initial condition
+  context "with genetic code 'DDCCCCDCDDD'" do
+    it "should defect initially" do
+      player = GeneticPlayer.new('DDCCCCDCDDD')
+      player.decision.should eql :defection
+    end
   end
 end
