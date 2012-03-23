4af25de57c73b64124fe65bb6fd5590276b2cea1
aaz <akbar.zamir@gmail.com>
2012-03-23 17:11:45 +0000

    Added a failing test (marked pending) for player faces twin (#198885)

--- spec/tournament_spec.rb
+++ spec/tournament_spec.rb
@@ -1,5 +1,6 @@
 require 'tournament'
 require 'tit4tat'
+require 'defector'
 
 describe Tournament do
   context 'with 5 tit4tat players' do
@@ -13,10 +14,21 @@ describe Tournament do
       @tournament.contests.size.should == 10
     end
     it 'should result in 30 points per player' do
-      @tournament.contests.each do |contest|
-        contest.player_one_score.should == 30
-        contest.player_two_score.should == 30
+      @tournament.contests.keys.each do |key|
+        @tournament.contests[key].player_one_score.should == 30
+        @tournament.contests[key].player_two_score.should == 30
       end
+    end
+  end
+  context 'with three players' do
+    it 'should have a result for the contest between a strategy and its twin' do
+      pending
+      defector = Defector.new
+      tit4tat = Tit4Tat.new
+      players = [defector, tit4tat]
+      tournament = Tournament.new(players)
+      tournament.run
+      tournament.contests.should have_key([defector.name, defector.name])
     end
   end
 end
--- tournament.rb
+++ tournament.rb
@@ -3,7 +3,7 @@ require_relative 'contest'
 class Tournament
   def initialize(players)
     @players = players
-    @contests = []
+    @contests = {}
   end
   
   def contests
@@ -12,12 +12,19 @@ class Tournament
   
   def run
     # Round robin contests
+#    for i in 0..(@players.size-2) do
+#      for j in (i+1)..(@players.size-1) do
+#        @contests[[@players[i], @players[j]]] = Contest.new(@players[i], @players[j], 10)
+#        @contests[[@players[i], @players[j]]].play
+#      end
+#    end
+    
     while (@players.size >= 2) do
       player = @players.shift
       @players.each do |opponent|
         contest = Contest.new(player, opponent, 10)
         contest.play
-        @contests.push contest
+        @contests[[player.name, opponent.name]] = contest
       end
     end
   end
