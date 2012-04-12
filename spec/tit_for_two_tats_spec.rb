c6221938b980933fd1cc599a7ae33e4a414aa20e
aaz <akbar.zamir@gmail.com>
2012-04-12 17:53:18 +0100

    First draft of Tit For Two Tats (#219464)

--- spec/tit_for_two_tats_spec.rb
+++ spec/tit_for_two_tats_spec.rb
@@ -5,12 +5,26 @@ describe 'TitForTwoTats' do
     @player = TitForTwoTats.new
   end
   context 'in the first instance' do
-    it 'should cooperate'
+    it 'should cooperate' do
+      @player.choice.should eql :cooperate
+    end
   end
   context 'when confronted with an isolated defection' do
-    it 'should continue to cooperate'
+    it 'should continue to cooperate' do
+      @player.experience :cooperate
+      @player.experience :defect
+      @player.choice.should eql :cooperate
+      @player.experience :cooperate
+      @player.choice.should eql :cooperate
+    end
   end
   context 'when confronted with two consecutive defections' do
-    it 'should retaliate'
+    it 'should retaliate' do
+      @player.experience :cooperate
+      @player.experience :defect
+      @player.choice.should eql :cooperate
+      @player.experience :defect
+      @player.choice.should eql :defect
+    end
   end
 end
--- tit_for_two_tats.rb
+++ tit_for_two_tats.rb
@@ -2,5 +2,13 @@ require 'player'
 
 class TitForTwoTats
   include Player
-  
+
+  def choice
+    if @memory_of_other_player.size > 1 and 
+        @memory_of_other_player.last(2) == [:defect, :defect] then
+      return :defect
+    else
+      return :cooperate
+    end
+  end
 end
